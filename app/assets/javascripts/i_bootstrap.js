3c25827383fc99369d194d87b2300431c2f2d2a0
Devin Zhang <daqing1986@gmail.com>
2012-12-08 19:28:08 +0800

    add jquery effects lib

--- app/assets/javascripts/i_bootstrap.js
+++ app/assets/javascripts/i_bootstrap.js
@@ -5,6 +5,8 @@
 //= require jquery_ui_mouse
 //= require jquery_ui_sortable
 //= require twitter/bootstrap
+//= require jquery_effects_core
+//= require jquery_effects_highlight
 //= require jquery_elastic
 //= require jquery.facebox
 //= require jquery_at_caret
