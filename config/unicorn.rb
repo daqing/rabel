37e321ce3beb21a2e0137a06d69d27774c2d7528
Devin Zhang <daqing1986@gmail.com>
2013-02-19 20:47:50 +0800

    update unicorn config
    don't listen on port 3000

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -29,7 +29,6 @@ working_directory APP_HOME # available in 0.94.0+
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
 listen "#{APP_HOME}/tmp/sockets/unicorn.sock", :backlog => 64
-listen 3000, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
