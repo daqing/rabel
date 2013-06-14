7dd34cf5f71d59552bb63d145c07fa215b4c198c
Devin Zhang <daqing1986@gmail.com>
2013-06-14 10:37:17 +0800

    tweak configuration

--- deploy/start_unicorn_manually.sh
+++ deploy/start_unicorn_manually.sh
@@ -2,7 +2,7 @@
 DIR=$( cd "$( dirname "$0" )" && pwd )
 source $DIR/shared.sh
 
-bundle exec unicorn -c ./config/unicorn.rb -E production -D
+bundle exec unicorn -c `pwd`/config/unicorn.rb -E production -D
 
 if [[ "$?" == 0 ]]; then
   log_info "unicorn master started."
