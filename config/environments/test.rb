f1aecff429d61cce8cdafca37274f47cb13fc031
ikeqiao <zhzsi@126.com>
2014-06-12 13:39:06 +0800

    升级到rails 4.0,可以运行

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,7 +6,9 @@ Rabel::Application.configure do
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs.  Don't rely on the data there!
   config.cache_classes = true
-
+  
+  # Do not eager load code on boot.
+  config.eager_load = false
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
