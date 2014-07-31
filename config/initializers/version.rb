7eb0dfb7cf9391e5ab8ef892e82ee9d13602fb21
Devin Zhang <daqing1986@gmail.com>
2014-07-31 12:39:21 +0800

    bump to version 1.5.3

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,9 +1,9 @@
 module Rabel
   module VERSION #:nodoc:
     MAJOR = 1
-    MINOR = 6
-    TINY  = 0
-    PRE   = 'dev'
+    MINOR = 5
+    TINY  = 3
+    PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
