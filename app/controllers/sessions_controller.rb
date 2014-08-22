044e98c17192ea2cabcef12d22dcdf7421aedf1a
Devin Zhang <daqing1986@gmail.com>
2014-08-22 11:21:13 +0800

    fix javascript asset path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,10 +13,6 @@ class SessionsController < Devise::SessionsController
     sign_in(resource_name, resource)
     reset_session
     session.merge!(old_session)
-    if mobile_device?
-      redirect_to root_path
-    else
-      respond_with resource, :location => after_sign_in_path_for(resource)
-    end
+    respond_with resource, :location => after_sign_in_path_for(resource)
   end
 end
