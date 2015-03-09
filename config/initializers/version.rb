5daa060c23311a05c65f7a36841a03ece5c46eef
Devin Zhang <daqing1986@gmail.com>
2015-03-09 16:03:54 +0800

    start working on v2.0

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,9 +1,9 @@
 module Rabel
   module VERSION #:nodoc:
-    MAJOR = 1
-    MINOR = 5
-    TINY  = 4
-    PRE   = 6
+    MAJOR = 2
+    MINOR = 0
+    TINY  = 0
+    PRE   = 'alpha'
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
