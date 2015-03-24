86fa411b849406dea8d11c31d11daeced204fbad
Devin Zhang <daqing1986@gmail.com>
2015-03-24 11:43:33 +0800

    fix bug related to migration

--- app/controllers/admin/channels_controller.rb
+++ app/controllers/admin/channels_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class Admin::ChannelsController < Admin::BaseController
   def index
     @channels = Channel.default_order.all
--- db/migrate/20150324010453_remove_node_id_from_topics.rb
+++ db/migrate/20150324010453_remove_node_id_from_topics.rb
@@ -1,6 +1,5 @@
 class RemoveNodeIdFromTopics < ActiveRecord::Migration
   def change
-    remove_index :topics, :node_id
     remove_column :topics, :node_id, :integer
   end
 end
