f0d2d2a3491c303dc90c195357181e0b914c3180
Devin Zhang <daqing1986@gmail.com>
2015-04-26 14:51:13 +0800

    Setup mailcatcher for development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {address: 'localhost', port: 1025}
 end
