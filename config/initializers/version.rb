783d2bac7deb466e7d2c818fce21b614b89bcd75
David Zhang <v8os.com@gmail.com>
2019-02-24 06:23:33 +0800

    Fix bootstrap-sass vulnerability

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,14 +1,5 @@
 module Rabel
-  module VERSION #:nodoc:
-    MAJOR = 2
-    MINOR = 0
-    TINY  = 0
-    PRE   = 'alpha'
-
-    STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
-  end
-
   def self.version
-    self::VERSION::STRING
+    "2.0.0.alpha"
   end
 end
