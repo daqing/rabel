820e0b20ea598060a9996eae1e41297afaed1035
David Zhang <v8os@icloud.com>
2023-04-17 23:32:04 +0800

    Upgrade to rails 7

--- config/initializers/business.rb
+++ config/initializers/business.rb
@@ -1,3 +1,5 @@
-Fanli.on(:create_topic, NotifyMentionedUsers)
-Fanli.on(:create_comment, NotifyMentionedUsers, TouchCommentable)
-Fanli.on(:destroy_comment, TouchCommentable)
+# FIXME: use service instead
+
+# Fanli.on(:create_topic, NotifyMentionedUsers)
+# Fanli.on(:create_comment, NotifyMentionedUsers, TouchCommentable)
+# Fanli.on(:destroy_comment, TouchCommentable)
