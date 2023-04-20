a71ed43763519c5e95c3fb99ae3d4763a30df79a
David Zhang <v8os@icloud.com>
2023-04-20 15:06:01 +0800

    Remove `custom_logo` settings

--- config/initializers/siteconf.rb
+++ config/initializers/siteconf.rb
@@ -6,7 +6,7 @@
 # Siteconf.defaults[:custom_css] = '' # 全局自定义CSS
 # Siteconf.defaults[:custom_js] = '' # 全局自定义JavaScript
 # Siteconf.defaults[:custom_head_tags] = '' # 自定义Head标签
-# Siteconf.defaults[:custom_logo] = ''
+
 # Siteconf.defaults[:global_banner] = ''
 
 # Siteconf.defaults[:allow_markdown_in_topics] = 'off'
