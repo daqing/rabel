2ef156f6536925bfb0abc026e127c46f86b93ef6
David Zhang <daqing@mindgee.io>
2024-04-19 16:24:31 +0800

    Remove devise related code

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,25 +1,5 @@
-# encoding: utf-8
-class RegistrationsController < Devise::RegistrationsController
-  layout 'single-column'
-
-  def create
-    build_resource(user_params)
-
-    if resource.verify_captcha(session[:captcha]) and resource.save
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_in(resource_name, resource)
-        respond_with resource, :location => after_sign_up_path_for(resource)
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-        expire_session_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords resource
-      respond_with resource
-    end
-  end
+class RegistrationsController
+  def create; end
 
   private
 
@@ -30,5 +10,4 @@ class RegistrationsController < Devise::RegistrationsController
                                  :nickname,
                                  :captcha)
   end
-
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,5 @@
-class SessionsController < Devise::SessionsController
-  layout 'single-column'
+class SessionsController
+  def new; end
 
-  def new
-    @title = "登入 - #{Siteconf.site_name}"
-    super
-  end
-
-  def create
-    old_session = session.dup
-    resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")
-    sign_in(resource_name, resource)
-    reset_session
-    session.merge!(old_session)
-    respond_with resource, :location => after_sign_in_path_for(resource)
-  end
+  def create; end
 end
