911b7f0d2a017c2936dabf5d50a4a1e7cdcaffe2
David Zhang <daqing@mindgee.io>
2024-04-20 01:28:58 +0800

    Improve db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,15 @@
-[
-  [I18n.t("navlink.crypto"), "/crypto"],
-  [I18n.t("navlink.forex"), "/forex"],
-  [I18n.t("navlink.gold_silver"), "/gold-silver"],
-  [I18n.t("navlink.china_a_shares"), "/china-a-shares"],
-  [I18n.t("navlink.us_stocks"), "/us-stocks"],
-  [I18n.t("navlink.hk_stocks"), "/hong-kong-stocks"],
-  [I18n.t("navlink.futures"), "/futures"]
-].each do |title, url|
-  NavLink.create!(title:, url:)
+def create_dqpub_navlink
+  [
+    [I18n.t("navlink.dqpub.crypto"), "/crypto"],
+    [I18n.t("navlink.dqpub.forex"), "/forex"],
+    [I18n.t("navlink.dqpub.gold_silver"), "/gold-silver"],
+    [I18n.t("navlink.dqpub.china_a_shares"), "/china-a-shares"],
+    [I18n.t("navlink.dqpub.us_stocks"), "/us-stocks"],
+    [I18n.t("navlink.dqpub.hk_stocks"), "/hong-kong-stocks"],
+    [I18n.t("navlink.dqpub.futures"), "/futures"]
+  ].each do |title, url|
+    NavLink.create!(title:, url:)
+  end
 end
+
+# create_dqpub_navlink
