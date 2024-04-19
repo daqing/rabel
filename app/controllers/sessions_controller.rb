ae05f61563a48a4fcb1e1ca2c36491b761645d23
David Zhang <daqing@mindgee.io>
2024-04-19 17:08:22 +0800

    User sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,17 @@
 class SessionsController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    @user = User.find_by(nickname: params[:session][:nickname].strip)
+
+    if @user&.authenticate(params[:session][:password])
+      sign_in(@user)
+
+      redirect_to root_path
+    else
+      respond_to(&:turbo_stream)
+    end
+  end
 
   def destroy
     sign_out
