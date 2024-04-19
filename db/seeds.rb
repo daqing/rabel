25d3d1a49533426ea085b383dfbb29281435e593
David Zhang <daqing@mindgee.io>
2024-04-20 01:45:23 +0800

    Create default nav links

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,4 +12,18 @@ def create_dqpub_navlink
   end
 end
 
+def create_default_navlinks
+  [
+    ["论坛", "/forum"],
+    ["成员", "/members"],
+    ["动态", "/feeds"],
+    ["聊天室", "/chat"],
+    ["关于", "/about"]
+  ].each do |title, url|
+    NavLink.create!(title:, url:)
+  end
+end
+
+create_default_navlinks
+
 # create_dqpub_navlink
