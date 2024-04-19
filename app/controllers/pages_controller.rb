64fd1b83548ce6537a02b1c07efeecd25e9f8120
David Zhang <daqing@mindgee.io>
2024-04-20 04:25:28 +0800

    Update pages page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,11 @@
 class PagesController < ApplicationController
   def show
-    if current_user && current_user.can_manage_site?
-      @page = Page.find_by_key!(params[:key])
-    else
-      @page = Page.where(:published => true).find_by!(:key => params[:key])
-    end
+    @page = if current_user&.can_manage_site?
+              Page.find_by_key!(params[:key])
+            else
+              Page.where(published: true).find_by!(key: params[:key])
+            end
 
     @title = @page.title
-
-    render layout: 'single-column'
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,12 @@ def create_default_navlinks
     ["成员", "/people"],
     ["动态", "/feed"],
     ["聊天室", "/chat"],
-    ["关于", "/about"]
+    ["关于", "/page/about"]
   ].each do |title, url|
     NavLink.create!(title:, url:)
   end
 end
 
 create_default_navlinks
+
+Page.create!(key: "about", title: "关于", content: "关于我们的介绍", published: true)
